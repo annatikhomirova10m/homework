737d96c2735b039275ba3b7537d29a2b4521bd89
annatikhomirova10m <43827224+annatikhomirova10m@users.noreply.github.com>
2018-12-26 19:01:00 +0300

    Create 11.cpp

---
+++ 2018.12.26/11.cpp
@@ -0,0 +1,34 @@
+//Напишите программу, находящую в массиве такой элемент, что равные ему составляют в массиве - большинство. Если такого элемента нет - выведите “NO”
+
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+int n,s,d;
+printf("enter size of mass\n");
+scanf("%d",&n);
+int a[n];
+for (int i=0;i<n;i++)
+scanf("%d",&a[i]);
+for (int k=0;k<n;k++)
+printf("a[%d]=%d\n",k,a[k]);
+d=0;
+for (int m=0;m<n;m++)
+{
+s=0;
+for (int l=0;l<n;l++)
+{
+if (a[m]==a[l])
+s++;
+if (s>(n/2))
+d=a[m];
+}
+}
+if (d!=0)
+printf("%d\n",d);
+else
+printf("NO");
+return 0;
+}
