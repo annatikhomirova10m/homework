973dae927f6ff8020182080011278fc18808df45
annatikhomirova10m <43827224+annatikhomirova10m@users.noreply.github.com>
2018-12-26 19:32:19 +0300

    Create 13.cpp

---
+++ 2018.12.26/13.cpp
@@ -0,0 +1,38 @@
+// Дан набор отрезков на прямой, заданный концами этих отрезков (a[i],b[i]). Напишите программу находящую наибольшее подмножество отрезков из набора, такое что элементы этого множества не пересекаются
+
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+int l = 0;
+void F(int *a, int *b, int a0, int max)
+{
+int min = b[0];
+int k = 0;
+for(int i = 0; i < n; i++)
+if(min > b[i] && b[i] => a0)
+{
+min = b[i];
+k = i;
+}
+a0 = a[j];
+l++;
+if(a0 == max) cout << l;
+F(a, b, a0, max);
+}
+int main() 
+{
+int n; 
+cin >> n;
+int *a = new int[n];
+int *b = new int[n];
+for(int i = 0; i < n; i++)
+{
+cout << "start: "; cin >> a[i];
+cout << "finish: "; cin >> b[i];
+}
+int max = a[0];
+int j = 0;
+for(int i = 1; i < n; i++) if(max < a[i]) max = a[i];
+return 0;
+}
