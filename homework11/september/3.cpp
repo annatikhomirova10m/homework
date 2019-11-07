5cef67693826e8c8c701cd52d74a99bb26fb3204
annatikhomirova10m <43827224+annatikhomirova10m@users.noreply.github.com>
2019-11-07 20:41:11 +0300

    Create 3.cpp

---
+++ homework11/september/3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <string>
+int main()
+{
+int N=5;
+int k=3;
+int l=4;
+int v1,v2,v3,v4;
+vector<vector<int>> G(n);
+while (N+1)
+{
+for (int v=0;v<n+1;v++){
+for (int i=0;i<n+1;i++)
+G[i].push_back(v);
+}
+N—;
+}
+vector<vector<int>> G2(n);
+while (N+1)
+{
+for (int v=0;v<n+1;v++){
+for (int i=0;i<n+1;i++){
+G2[i].push_back(v);
+if(i==2) v1=i;
+if(i==3) v2=i;
+if(i==4) v3=i;
+if(i==5) v4=i;
+}
+}
+}
+N—;
+}
+G[1].push_back(v1);
+G[2].push_back(v2);
+G[3].push_back(v3);
+G[3].push_back(v4);
+}
